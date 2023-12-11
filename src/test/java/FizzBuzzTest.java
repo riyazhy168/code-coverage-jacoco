f1d7e2e20ca5d8af3ea0735afd41e67782073d02
riyazhy168 <riyazhy168@gmail.com>
2023-12-11 14:47:56 +0530

    testCommit

---
+++ src/test/java/FizzBuzzTest.java
@@ -0,0 +1,28 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertNull;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Petri Kainulainen
+ */
+public class FizzBuzzTest {
+
+    private FizzBuzz fizzBuzz;
+
+    @Before
+    public void setUp() {
+        fizzBuzz = new FizzBuzz();
+    }
+
+    @Test
+    public void getFizzBuzzWord_NumberIsMultipleOfThree_ShouldReturnFizz() {
+        assertThat(fizzBuzz.getFizzBuzzWord(3), is("Fizz"));
+    }
+//    @Test
+//    public void testOne(){
+//        assertThat(fizzBuzz.returnOne(), is(1));
+//    }
+}
