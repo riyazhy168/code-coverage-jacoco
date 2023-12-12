0aa92416cebf9d76517ca8bd1e332642159e16d0
riyazhy168 <riyazhy168@gmail.com>
2023-12-12 09:27:19 +0530

    testCommit

---
+++ src/main/java/ReturnNumbers.java
@@ -0,0 +1,21 @@
+public class ReturnNumbers {
+    public Integer returnOne() {
+        return 1;
+    }
+
+    public Integer return2() {
+        return 2;
+    }
+
+    public Integer return3() {
+        return 3;
+    }
+
+    public Integer return4() {
+        return 4;
+    }
+
+    public Integer return5() {
+        return 5;
+    }
+}
